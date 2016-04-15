17c7f1017dbd2c74196b217a93d19c98384abd06
chenbojun <hzchenbojun@corp.netease.com>
2016-04-15 10:56:05 +0800

    "Test Android Studio git"

--- app/src/main/java/com/example/hzchenbojun/doubanreader/home/views/MainActivity.java
+++ app/src/main/java/com/example/hzchenbojun/doubanreader/home/views/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements MainView {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        //123
     }
 
     private void init() {
